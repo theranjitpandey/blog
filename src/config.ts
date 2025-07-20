1942afbd7e8244ce80a30fc18a7e730b7f2498ee
theranjitpandey <67914991+theranjitpandey@users.noreply.github.com>
2025-07-20 16:24:21 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -73,7 +73,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/theranijtpandey",
+			url: "https://github.com/theranjitpandey",
 		},
 	],
 };

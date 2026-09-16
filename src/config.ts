6d0ce2628f1295b78f24bcd59623e62032b75835
theranjitpandey <67914991+theranjitpandey@users.noreply.github.com>
2026-09-16 21:57:52 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -56,7 +56,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Ranjit Pandey",
-	bio: "ꜱᴏꜰᴛᴡᴀʀᴇ ᴇɴɢɪɴᴇᴇʀ || ᴘʏᴛʜᴏɴ ᴅᴇᴠᴇʟᴏᴘᴇʀ",
+	bio: "ꜱᴄɪᴇɴᴄᴇ ꜰᴏʀ ʙᴇᴛᴛᴇʀ ʙᴇɪɴɢ 🧠 ᴍɪɴᴅ • ɴᴇᴜʀᴏꜱᴄɪᴇɴᴄᴇ • ʀᴇꜱᴇᴀʀᴄʜ 💻 ꜱᴏꜰᴛᴡᴀʀᴇ • ʙᴏᴏᴋꜱ • ᴡɪꜱᴅᴏᴍ ⌬ ᴋɴᴏᴡʟᴇᴅɢᴇ. ᴅɪꜱᴄɪᴘʟɪɴᴇ. ʟɪꜰᴇꜱᴛʏʟᴇ.",
 	links: [
 		{
 			name: "Instagram",
@@ -72,10 +72,14 @@ export const profileConfig: ProfileConfig = {
 		},
 		
 		{
-			name: "GitHub",
-			icon: "fa6-brands:github",
-			url: "https://github.com/theranjitpandey",
+			name: "Linkedin",
+			icon: "fa6-brands:linkedin-in", // Visit https://icones.js.org/ for icon codes
+			// You will need to install the corresponding icon set if it's not already included
+			// `pnpm add @iconify-json/<icon-set-name>`
+			url: "www.linkedin.com/in/ranjitpandey",
+			
 		},
+		
 	],
 };
 
